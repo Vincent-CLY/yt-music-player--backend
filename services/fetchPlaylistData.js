78e9b1979c4604f249a3ebc2c0c24f1aa75d6347
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-17 03:42:11 +0800

    Change from require() to import

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -1,21 +1,24 @@
-import axios from "axios"
-import Innertube from "youtubei.js"
-const youtube = await Innertube.create()
-const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc" // Just for testing
+import axios from "axios";
+import Innertube from "youtubei.js";
+const youtube = await Innertube.create();
+const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID) {
-    let playlist = await youtube.getPlaylist(playlistID)
-    let playlistItems = Array.from(playlist.items)
-    console.log(playlistItems.length) // 100
+  let playlist = await youtube.getPlaylist(playlistID);
+  let playlistItems = Array.from(playlist.items);
+  console.log(playlist.info.total_items); // ['items', 'has_continuation', 'continuation']
+  console.log(playlist.items);
+  for (let i = 0; i < playlist.info.total_items; i++) {
+    
+  }
+  // fetch all data until the end
+  while (playlist.has_continuation) {
+    playlist = await playlist.getContinuation();
 
-    // fetch all data until the end
-    while (playlist.has_continuation) {
-    playlist = await playlist.getContinuation()
+    playlistItems = playlistItems.concat(playlist.items);
+  }
 
-    playlistItems = playlistItems.concat(playlist.items)
-    }
-
-    console.log(playlistItems.length) // 598
+  console.log(playlistItems.length); // 598
 }
 
 // Use ES module export
