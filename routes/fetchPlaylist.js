5bc87f6f94b6786fc044f7cfea955ce6295b46e3
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-16 21:37:56 +0000

    Updated fetchPlaylist.js

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -7,14 +7,7 @@ router.get("/playlists/:id", async (req, res) => {
   const playlistId = req.params.id
   try {
     const data = await fetchPlaylistData(playlistId);
-    res.send(`
-      <html>
-      <body>
-        <h1>Fetched Data for Playlist ID: ${playlistId}</h1>
-        <pre>${JSON.stringify(data, null, 2)}</pre> <!-- Display data in browser -->
-      </body>
-      </html>
-    `);
+    res.json(data)
   } catch (error) {
     console.error(`Error fetching playlist data for ID: ${playlistId}`, error)
     res.status(500).json({ message: "Internal Server Error" })
