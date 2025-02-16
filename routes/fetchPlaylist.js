00f86389382e1a386bd9b278033fb3d8f8b309e3
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-16 21:20:57 +0000

    Updated fetchPlaylist.js

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -1,17 +1,18 @@
-import express from "express";
-import fetchPlaylistData from "../services/fetchPlaylistData.js";
+import express from "express"
+import fetchPlaylistData from "../services/fetchPlaylistData.js"
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/playlists/:id", async (req, res) => {
-  const playlistId = req.params.id;
+  const playlistId = req.params.id
   try {
     const data = await fetchPlaylistData(playlistId);
-    res.json(data);
+    console.log(data)
+    res.json(data)
   } catch (error) {
-    console.error(`Error fetching playlist data for ID: ${playlistId}`, error);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.error(`Error fetching playlist data for ID: ${playlistId}`, error)
+    res.status(500).json({ message: "Internal Server Error" })
   }
-});
+})
 
 export default router;
