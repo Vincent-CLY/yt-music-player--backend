6d2d9721628f8673d7e9f3e2da25ec19f31f9e77
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-17 03:41:49 +0800

    Change from require() to import (Fix)

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -1,25 +1,16 @@
-import axios from "axios";
-import Innertube from "youtubei.js";
-const youtube = await Innertube.create();
-const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
+import express from "express";
+import fetchPlaylistData from "../services/fetchPlaylistData.js";
 
-async function fetchPlaylistData(playlistID) {
-  let playlist = await youtube.getPlaylist(playlistID);
-  let playlistItems = Array.from(playlist.items);
-  console.log(playlist.info.total_items); // ['items', 'has_continuation', 'continuation']
-  console.log(playlist.items);
-  for (let i = 0; i < playlist.info.total_items; i++) {
-    
+const router = express.Router();
+
+router.get("/playlists/:id", async (req, res) => {
+  const playlistId = req.params.id;
+  try {
+    const data = await fetchPlaylistData(playlistId);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  // fetch all data until the end
-  while (playlist.has_continuation) {
-    playlist = await playlist.getContinuation();
+});
 
-    playlistItems = playlistItems.concat(playlist.items);
-  }
-
-  console.log(playlistItems.length); // 598
-}
-
-// Use ES module export
-export default fetchPlaylistData;
+export default router;
