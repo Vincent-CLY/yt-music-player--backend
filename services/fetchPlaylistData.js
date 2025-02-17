1e3187e63d1bd70fe7aa4f299a43d32d5888317f
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-17 07:13:05 +0000

    Updated fetchPlaylistData.js return values

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -14,7 +14,13 @@ async function fetchPlaylistData(playlistID) {
   }
   
   console.log(playlistItems.length);
-  return playlistItems
+  return playlistItems.map(item => ({
+    id: item.id,
+    title: item.title,
+    thumbnail: item.thumbnail,
+    author: item.author,
+    duration: item.duration
+  }))
 }
 
 // Use ES module export
