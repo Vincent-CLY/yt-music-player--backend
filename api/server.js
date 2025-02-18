420a23c10759fc80f39153f53b90822e25413fc6
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-18 19:24:23 +0000

    Remove the cors from the server.js and add to the fetchPlaylist.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import apiFetchPlaylist from "../routes/fetchPlaylist.js";
 
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors({origin: 'https://vitejsviteeestp8it-y5f1--5173--7f809d15.local-corp.webcontainer.io'}));
 
 app.get("/", (req, res) => {
   res.send("Hello World in Vercel!");
