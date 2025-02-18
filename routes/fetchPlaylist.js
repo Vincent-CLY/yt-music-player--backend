a19da702ccc23233d74f84396641c2310ac555cd
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-18 19:39:34 +0000

    Removed use cors in fetchPlaylist.js

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -1,10 +1,7 @@
 import express from "express"
 import fetchPlaylistData from "../services/fetchPlaylistData.js"
 
-const cors = require('cors');
 const router = express.Router()
-
-router.use(cors({origin: 'https://vitejsviteeestp8it-y5f1--5173--7f809d15.local-corp.webcontainer.io'}));
 
 router.get("/playlists/:id", async (req, res) => {
   const playlistId = req.params.id
