874af983cb19e339fff5b9271d114b984d6098bd
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-18 19:05:47 +0000

    Update the server.js to use npm cors

--- api/server.js
+++ api/server.js
@@ -1,14 +1,11 @@
 import express from "express";
 import apiFetchPlaylist from "../routes/fetchPlaylist.js";
 
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World in Vercel!");
