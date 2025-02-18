4e6ad19c67195af2f27800e0a024e60fb4d6ab2c
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-18 18:59:14 +0000

    Updated server.js to accept CORS (All origins for test purpose)

--- api/server.js
+++ api/server.js
@@ -4,6 +4,12 @@ import apiFetchPlaylist from "../routes/fetchPlaylist.js";
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World in Vercel!");
 });
