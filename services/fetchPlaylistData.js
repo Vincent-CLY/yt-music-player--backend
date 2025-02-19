9deb8ed928907d443a6c3c7fc04dd996cc4d635b
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-19 14:15:48 +0000

    Testing Youtube.js returned data

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -6,12 +6,11 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 async function fetchPlaylistData(playlistID) {
   let playlist = await youtube.getPlaylist(playlistID);
   let playlistItems = Array.from(playlist.items);
-  console.log(playlist.items)
+  console.log(playlist.videos)
   // fetch all data until the end
   while (playlist.has_continuation) {
     playlist = await playlist.getContinuation();
     playlistItems = playlistItems.concat(playlist.items);
-    console.log(playlist.items)
   }
 
   console.log(playlistItems.length);
