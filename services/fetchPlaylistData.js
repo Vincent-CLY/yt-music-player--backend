1be9da5bcc24df2fd9472aa50e28721819126754
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-19 18:16:55 +0000

    testing return speed

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -6,7 +6,6 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 async function fetchPlaylistData(playlistID) {
   let playlist = await youtube.getPlaylist(playlistID);
   let playlistItems = Array.from(playlist.items);
-  console.log(playlist.info.total_items)
   // fetch all data until the end
   while (playlist.has_continuation) {
     playlist = await playlist.getContinuation();
@@ -14,13 +13,13 @@ async function fetchPlaylistData(playlistID) {
   }
 
   console.log(playlistItems.length);
-  return playlistItems.map(item => ({
-    id: item.id,
-    title: item.title.text,
-    thumbnail: item.thumbnails[0],
-    author: item.author.name,
-    duration: item.duration.seconds
-  }))
+  // return playlistItems.map(item => ({
+  //   id: item.id,
+  //   title: item.title.text,
+  //   thumbnail: item.thumbnails[0],
+  //   author: item.author.name,
+  //   duration: item.duration.seconds
+  // }))
 }
 
 // Use ES module export
