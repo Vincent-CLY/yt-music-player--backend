d54fcc614909d427e4a326128ee6647ac4bdb159
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-19 13:51:08 +0000

    Testing returned json content

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -6,10 +6,11 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 async function fetchPlaylistData(playlistID) {
   let playlist = await youtube.getPlaylist(playlistID);
   let playlistItems = Array.from(playlist.items);
-  return playlistItems;
   // fetch all data until the end
   while (playlist.has_continuation) {
     playlist = await playlist.getContinuation();
+    return playlist;
+    
     
     playlistItems = playlistItems.concat(playlist.items);
   }
