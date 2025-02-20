d0430054da28047e9cbea4911447e70e154b3205
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-20 03:39:50 +0000

    Test performance of not returning length

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -5,27 +5,21 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID) {
   let playlist = await youtube.getPlaylist(playlistID);
-  let playlistItems = Array.from(playlist.items.map(item => ({
+  let playlistItems = Array.from(playlist.items);
+
+  // fetch all data until the end
+  while (playlist.has_continuation) {
+    playlist = await playlist.getContinuation();
+    playlistItems = playlistItems.concat(playlist.items);
+  }
+  return playlistItems.length;
+  playlistItems.map(item => ({
     id: item.id,
     title: item.title.text,
     thumbnail: item.thumbnails[0],
     author: item.author.name,
     duration: item.duration.seconds
-  })));
-
-  // fetch all data until the end
-  while (playlist.has_continuation) {
-    playlist = await playlist.getContinuation();
-    playlistItems = playlistItems.concat(playlist.items.map(item => ({
-      id: item.id,
-      title: item.title.text,
-      thumbnail: item.thumbnails[0],
-      author: item.author.name,
-      duration: item.duration.seconds
-    })));;
-  }
-  return playlistItems;
-
+  }));
   console.log(playlistItems.length);
 }
 
