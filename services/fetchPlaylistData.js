35910d5c6018085d868dc1dc05404f3d05048356
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-20 03:42:34 +0000

    Testing to use counter for performance comparing

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -5,14 +5,15 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID) {
   let playlist = await youtube.getPlaylist(playlistID);
-  let playlistItems = Array.from(playlist.items);
-
+  // let playlistItems = Array.from(playlist.items);
+  let counter = playlist.items.length;
   // fetch all data until the end
   while (playlist.has_continuation) {
-    playlist = await playlist.getContinuation();
-    playlistItems = playlistItems.concat(playlist.items);
+    playlist = await playlist.getContinuationData();
+    // playlistItems = playlistItems.concat(playlist.items);
+    counter += playlist.length;
   }
-  return playlistItems.length;
+  return counter;
   playlistItems.map(item => ({
     id: item.id,
     title: item.title.text,
