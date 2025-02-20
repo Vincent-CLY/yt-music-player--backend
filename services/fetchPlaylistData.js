211de7bf49193f2e0b699f0a83fc9af4437682e0
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-20 03:48:58 +0000

    Map before final return

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -5,24 +5,27 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID) {
   let playlist = await youtube.getPlaylist(playlistID);
-  // let playlistItems = Array.from(playlist.items);
-  let counter = playlist.items.length;
-  console.log(playlist.getContinuationData());
-  // fetch all data until the end
-  while (playlist.has_continuation) {
-    playlist = await playlist.getContinuation();
-    
-    // playlistItems = playlistItems.concat(playlist.items);
-    counter += playlist.items.length;
-  }
-  return counter;
-  playlistItems.map(item => ({
+  let playlistItems = Array.from(playlist.items.map(item => ({
     id: item.id,
     title: item.title.text,
     thumbnail: item.thumbnails[0],
     author: item.author.name,
     duration: item.duration.seconds
-  }));
+  })));
+
+  // fetch all data until the end
+  while (playlist.has_continuation) {
+    playlist = await playlist.getContinuation();
+    playlistItems = playlistItems.concat(playlist.items.map(item => ({
+      id: item.id,
+      title: item.title.text,
+      thumbnail: item.thumbnails[0],
+      author: item.author.name,
+      duration: item.duration.seconds
+    })));;
+  }
+  return playlistItems;
+
   console.log(playlistItems.length);
 }
 
