5341f0766d17325a84c70c4b43854594cc44614f
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-20 03:02:58 +0000

    Log the data length in vercel

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -8,6 +8,7 @@ router.get("/playlists/:id", async (req, res) => {
   try {
     const data = await fetchPlaylistData(playlistId);
     console.log(data);
+    console.log(data.length);
     res.json(data)
   } catch (error) {
     console.error(`Error fetching playlist data for ID: ${playlistId}`, error)
