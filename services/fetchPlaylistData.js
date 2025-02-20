1c9293e50adea564865ef3bba93ac1c8cd34282e
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-20 04:05:22 +0000

    Add console.time to log in vercel

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -4,6 +4,7 @@ const youtube = await Innertube.create();
 const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID) {
+  console.time("Fetching Time");
   let playlist = await youtube.getPlaylist(playlistID);
   let playlistItems = Array.from(playlist.items.map(item => ({
     id: item.id,
@@ -12,10 +13,14 @@ async function fetchPlaylistData(playlistID) {
     author: item.author.name,
     duration: item.duration.seconds
   })));
-
+  console.timeEnd("Fetching Time");
   // fetch all data until the end
   while (playlist.has_continuation) {
+    console.time("Total Execution Time");
+    console.time("Fetching Continuation");
     playlist = await playlist.getContinuation();
+    console.timeEnd("Fetching Continuation");
+    console.time('Concatenation Time');
     playlistItems = playlistItems.concat(playlist.items.map(item => ({
       id: item.id,
       title: item.title.text,
@@ -23,7 +28,10 @@ async function fetchPlaylistData(playlistID) {
       author: item.author.name,
       duration: item.duration.seconds
     })));;
+    console.timeEnd('Concatenation Time')
   }
+  console.log(`Total items fetched: ${playlistItems.length}`);
+  console.timeEnd("Total Execution Time");
   return playlistItems;
 
   console.log(playlistItems.length);
