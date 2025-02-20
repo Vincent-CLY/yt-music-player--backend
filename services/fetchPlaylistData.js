38d7df3d994294b6707fd6a4c7dc406ae08da76f
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-20 03:59:51 +0000

    Revert back to serial request

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -1,43 +1,32 @@
 import axios from "axios";
 import Innertube from "youtubei.js";
-
 const youtube = await Innertube.create();
 const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID) {
-  const firstPage = await youtube.getPlaylist(playlistID);
-  let playlistItems = firstPage.items.map(item => ({
+  let playlist = await youtube.getPlaylist(playlistID);
+  let playlistItems = Array.from(playlist.items.map(item => ({
     id: item.id,
     title: item.title.text,
     thumbnail: item.thumbnails[0],
     author: item.author.name,
     duration: item.duration.seconds
-  }));
+  })));
 
-  // Array to hold all continuation promises
-  const continuationPromises = [];
-  let nextPage = firstPage;
-  while (nextPage.has_continuation) {
-    continuationPromises.push(nextPage.getContinuation());
-    nextPage = await nextPage.getContinuation();
-  }
-
-  // Wait for all continuation requests to complete
-  const pages = await Promise.all(continuationPromises);
-
-  // Process each page and combine the results
-  pages.forEach(page => {
-    playlistItems = playlistItems.concat(page.items.map(item => ({
+  // fetch all data until the end
+  while (playlist.has_continuation) {
+    playlist = await playlist.getContinuation();
+    playlistItems = playlistItems.concat(playlist.items.map(item => ({
       id: item.id,
       title: item.title.text,
       thumbnail: item.thumbnails[0],
       author: item.author.name,
       duration: item.duration.seconds
-    })));
-  });
+    })));;
+  }
+  return playlistItems;
 
   console.log(playlistItems.length);
-  return playlistItems;
 }
 
 // Use ES module export
