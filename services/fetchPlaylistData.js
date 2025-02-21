aab03b744ae203155c97bc16ecc4690dab3d69ce
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-21 22:24:48 +0000

    Added {} to make the js function works inside console.log and res.write

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -5,8 +5,8 @@ const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID, res) {
   let playlist = await youtube.getPlaylist(playlistID);
-  console.log(`$JSON.stringify(playlist.info.total_items)\n`)
-  res.write(`$JSON.stringify(playlist.info.total_items)\n`)
+  console.log(`${JSON.stringify(playlist.info.total_items)}\n`)
+  res.write(`${JSON.stringify(playlist.info.total_items)}\n`)
   let playlistItems = Array.from(playlist.items.map(item => ({
     id: item.id,
     title: item.title.text,
