86dc76cc70c2fb75402902adb898b416732d30b8
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-21 07:16:21 +0000

    Used res.write and res.flus approach to attempt sse

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -6,10 +6,11 @@ const router = express.Router()
 router.get("/playlists/:id", async (req, res) => {
   const playlistId = req.params.id
   try {
-    const data = await fetchPlaylistData(playlistId);
-    console.log(data);
-    console.log(data.length);
-    res.json(data)
+    // const data = await fetchPlaylistData(playlistId, res);
+    // console.log(data);
+    // console.log(data.length);
+    // res.json(data)
+    await fetchPlaylistData(playlistId, res);
   } catch (error) {
     console.error(`Error fetching playlist data for ID: ${playlistId}`, error)
     res.status(500).json({ message: "Internal Server Error" })
--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -3,10 +3,10 @@ import Innertube from "youtubei.js";
 const youtube = await Innertube.create();
 const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
-async function fetchPlaylistData(playlistID) {
-  console.time("Total Execution Time");
-  console.time("Fetching Time");
+async function fetchPlaylistData(playlistID, res) {
   let playlist = await youtube.getPlaylist(playlistID);
+  res.write(plalylist.info.total_items);
+  res.flush();
   let playlistItems = Array.from(playlist.items.map(item => ({
     id: item.id,
     title: item.title.text,
@@ -14,13 +14,12 @@ async function fetchPlaylistData(playlistID) {
     author: item.author.name,
     duration: item.duration.seconds
   })));
-  console.timeEnd("Fetching Time");
+  res.write(playlistItems);
+  res.flush();
+
   // fetch all data until the end
   while (playlist.has_continuation) {
-    console.time("Fetching Continuation");
     playlist = await playlist.getContinuation();
-    console.timeEnd("Fetching Continuation");
-    console.time('Concatenation Time');
     playlistItems = playlistItems.concat(playlist.items.map(item => ({
       id: item.id,
       title: item.title.text,
@@ -28,10 +27,9 @@ async function fetchPlaylistData(playlistID) {
       author: item.author.name,
       duration: item.duration.seconds
     })));;
-    console.timeEnd('Concatenation Time')
+    res.write(playlistItems);
+    res.flush();
   }
-  console.log(`Total items fetched: ${playlistItems.length}`);
-  console.timeEnd("Total Execution Time");
   return playlistItems;
 
   console.log(playlistItems.length);
