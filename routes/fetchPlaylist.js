eb2a5f5d11d80ba115ece780d687c7b816d6b915
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-21 06:25:44 +0000

    Revert back to using serial data transfer

--- routes/fetchPlaylist.js
+++ routes/fetchPlaylist.js
@@ -4,33 +4,16 @@ import fetchPlaylistData from "../services/fetchPlaylistData.js"
 const router = express.Router()
 
 router.get("/playlists/:id", async (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  const sendData = (data) => {
-    res.write(`data: ${JSON.stringify(data)}\n\n`);
-  }
-
   const playlistId = req.params.id
   try {
-    // const data = await fetchPlaylistData(playlistId, sendData);
-    await fetchPlaylistData(playlistId, sendData);
-    // console.log(data);
-    // console.log(data.length);
-  //   res.json(data)
-  // } catch (error) {
-  //   console.error(`Error fetching playlist data for ID: ${playlistId}`, error)
-  //   res.status(500).json({ message: "Internal Server Error" })
-  // }
-    res.write('event: end\ndata: {}\n\n');
+    const data = await fetchPlaylistData(playlistId);
+    console.log(data);
+    console.log(data.length);
+    res.json(data)
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.write(`data: ${JSON.stringify({ error: 'Error fetching data' })}\n\n`);
+    console.error(`Error fetching playlist data for ID: ${playlistId}`, error)
+    res.status(500).json({ message: "Internal Server Error" })
   }
-  req.on('close', () => {
-    console.log('Client Disconnected');
-  })
 })
 
 export default router;
--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -3,42 +3,38 @@ import Innertube from "youtubei.js";
 const youtube = await Innertube.create();
 const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
-const extract = (items) => {
-  return items.map(item => ({
+async function fetchPlaylistData(playlistID) {
+  console.time("Total Execution Time");
+  console.time("Fetching Time");
+  let playlist = await youtube.getPlaylist(playlistID);
+  let playlistItems = Array.from(playlist.items.map(item => ({
     id: item.id,
     title: item.title.text,
     thumbnail: item.thumbnails[0],
     author: item.author.name,
     duration: item.duration.seconds
-  }))
-}
-
-async function fetchPlaylistData(playlistID, sendData) {
-  // console.time("Total Execution Time");
-  // console.time("Fetching Time");
-  let playlist = await youtube.getPlaylist(playlistID);
-  console.log(playlist.info.total_items);
-  sendData(playlist.info.total_items);
-  // let playlistItems = Array.from(extract(playlist.items));
-  console.log(extract(playlist.items));
-  sendData(extract(playlist.items));
-  // console.timeEnd("Fetching Time");
+  })));
+  console.timeEnd("Fetching Time");
   // fetch all data until the end
   while (playlist.has_continuation) {
-    // console.time("Fetching Continuation");
+    console.time("Fetching Continuation");
     playlist = await playlist.getContinuation();
-    // console.timeEnd("Fetching Continuation");
-    // console.time('Concatenation Time');
-    // playlistItems = playlistItems.concat(extract(playlist.items));
-    console.log(extract(playlist.items));
-    sendData(extract(playlist.items));
-    // console.timeEnd('Concatenation Time')
+    console.timeEnd("Fetching Continuation");
+    console.time('Concatenation Time');
+    playlistItems = playlistItems.concat(playlist.items.map(item => ({
+      id: item.id,
+      title: item.title.text,
+      thumbnail: item.thumbnails[0],
+      author: item.author.name,
+      duration: item.duration.seconds
+    })));;
+    console.timeEnd('Concatenation Time')
   }
-  // console.log(`Total items fetched: ${playlistItems.length}`);
-  // console.timeEnd("Total Execution Time");
-  // return playlistItems;
+  console.log(`Total items fetched: ${playlistItems.length}`);
+  console.timeEnd("Total Execution Time");
+  return playlistItems;
 
-  // console.log(playlistItems.length);
+  console.log(playlistItems.length);
 }
 
 // Use ES module export
