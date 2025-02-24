730208ebb4bce0f0505e819a3afdc0e9a66ece8a
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-02-24 09:23:06 +0000

    Added an error handler for playlist fetching

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -4,22 +4,11 @@ const youtube = await Innertube.create();
 const playlistID = "PLbHxd0f6XdHko_QS9nol7nzAxk64NRldc"; // Just for testing
 
 async function fetchPlaylistData(playlistID, res) {
-  let playlist = await youtube.getPlaylist(playlistID);
-  console.log(`data: ${JSON.stringify(playlist.info.total_items)}\n\n`)
-  res.write(`data: ${JSON.stringify(playlist.info.total_items)}\n\n`)
-  let playlistItems = Array.from(playlist.items.map(item => ({
-    id: item.id,
-    title: item.title.text,
-    thumbnail: item.thumbnails[0],
-    author: item.author.name,
-    duration: item.duration.seconds
-  })));
-  console.log(`data: ${JSON.stringify(playlistItems)}\n\n`)
-  res.write(`data: ${JSON.stringify(playlistItems)}\n\n`)
-  // fetch all data until the end
-  while (playlist.has_continuation) {
-    playlist = await playlist.getContinuation();
-    playlistItems = Array.from(playlist.items.map(item => ({
+  try {
+    let playlist = await youtube.getPlaylist(playlistID);
+    console.log(`data: ${JSON.stringify(playlist.info.total_items)}\n\n`)
+    res.write(`data: ${JSON.stringify(playlist.info.total_items)}\n\n`)
+    let playlistItems = Array.from(playlist.items.map(item => ({
       id: item.id,
       title: item.title.text,
       thumbnail: item.thumbnails[0],
@@ -28,6 +17,22 @@ async function fetchPlaylistData(playlistID, res) {
     })));
     console.log(`data: ${JSON.stringify(playlistItems)}\n\n`)
     res.write(`data: ${JSON.stringify(playlistItems)}\n\n`)
+    // fetch all data until the end
+    while (playlist.has_continuation) {
+      playlist = await playlist.getContinuation();
+      playlistItems = Array.from(playlist.items.map(item => ({
+        id: item.id,
+        title: item.title.text,
+        thumbnail: item.thumbnails[0],
+        author: item.author.name,
+        duration: item.duration.seconds
+      })));
+      console.log(`data: ${JSON.stringify(playlistItems)}\n\n`)
+      res.write(`data: ${JSON.stringify(playlistItems)}\n\n`)
+    }
+  } catch (error) {
+    console.log(error)
+    return error
   }
   // console.log(playlistItems.length);
 }
