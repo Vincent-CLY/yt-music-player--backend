68ea2606ff6a72c70df5af82d8c2389409c3ef74
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-12-04 20:12:07 +0000

    Remove Array.from()

--- services/fetchPlaylistData.js
+++ services/fetchPlaylistData.js
@@ -8,25 +8,25 @@ async function fetchPlaylistData(playlistID, res) {
     let playlist = await youtube.getPlaylist(playlistID);
     console.log(`data: ${JSON.stringify(playlist.info.total_items)}\n\n`)
     res.write(`data: ${JSON.stringify(playlist.info.total_items)}\n\n`)
-    let playlistItems = Array.from(playlist.items.map(item => ({
+    let playlistItems = playlist.items.map(item => ({
       id: item.id,
       title: item.title.text,
       thumbnail: item.thumbnails[0],
       author: item.author.name,
       duration: item.duration.seconds
-    })));
+    }));
     console.log(`data: ${JSON.stringify(playlistItems)}\n\n`)
     res.write(`data: ${JSON.stringify(playlistItems)}\n\n`)
     // fetch all data until the end
     while (playlist.has_continuation) {
       playlist = await playlist.getContinuation();
-      playlistItems = Array.from(playlist.items.map(item => ({
+      playlistItems = playlist.items.map(item => ({
         id: item.id,
         title: item.title.text,
         thumbnail: item.thumbnails[0],
         author: item.author.name,
         duration: item.duration.seconds
-      })));
+      }));
       console.log(`data: ${JSON.stringify(playlistItems)}\n\n`)
       res.write(`data: ${JSON.stringify(playlistItems)}\n\n`)
     }
